169e52097adf3475f8cf78961e253388abedea09
giapdz <giapdz104@gmail.com>
2022-01-17 00:15:12 +0700

    add middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
-import { Module } from '@nestjs/common';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 import { UsersController } from './users/users.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController, UsersController],
+  imports: [UsersModule],
+  controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes(UsersController);
+  }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,3 +1,4 @@
+import { UsersService } from './users.service';
 import {
   Controller,
   Get,
@@ -15,46 +16,57 @@ import {
   Delete,
 } from '@nestjs/common';
 import { Response, Request } from 'express';
-import { CreateUserDto } from './create-user.dto';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './interfaces/user.interface';
 
 @Controller('users')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto) {
+    this.usersService.create(createUserDto);
+  }
+  @Get()
+  async findAll(): Promise<User[]> {
+    return this.usersService.findAll();
+  }
   //   @Get()
   //   getInfo(@HostParam('account') account: string) {
   //     return account;
   //   }
 
-  @Post()
-  @Header('Cache-Control', 'none')
-  async create(@Body() creatUser: CreateUserDto) {
-    // return 'This action adds a new user';
-    return creatUser;
-  }
+  // @Post()
+  // @Header('Cache-Control', 'none')
+  // async create(@Body() creatUser: CreateUserDto) {
+  //   // return 'This action adds a new user';
+  //   return creatUser;
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return `This action removes a #${id} cat`;
-  }
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return `This action removes a #${id} cat`;
+  // }
 
-  @Get('docs')
-  @Redirect('https://docs.nestjs.com', 302)
-  getDocs(@Query('version') version) {
-    console.log(version);
-    if (version && version === '5') {
-      return { url: 'https://docs.nestjs.com/v5/' };
-    }
-  }
+  // @Get('docs')
+  // @Redirect('https://docs.nestjs.com', 302)
+  // getDocs(@Query('version') version) {
+  //   console.log(version);
+  //   if (version && version === '5') {
+  //     return { url: 'https://docs.nestjs.com/v5/' };
+  //   }
+  // }
 
-  @Get(':id')
-  findOne(@Param() params): string {
-    console.log(params.id);
-    return `This action returns a #${params.id} cat`;
-  }
+  // @Get(':id')
+  // findOne(@Param() params): string {
+  //   console.log(params.id);
+  //   return `This action returns a #${params.id} cat`;
+  // }
 
-  @Get()
-  async findAll(@Res() res: Response, @Req() req: Request) {
-    res.status(200).send(req.body);
+  // @Get()
+  // async findAll(@Res() res: Response, @Req() req: Request) {
+  //   res.status(200).send(req.body);
 
-    // return 'This action returns all cats';
-  }
+  //   // return 'This action returns all cats';
+  // }
 }
